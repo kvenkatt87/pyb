99c251e3092d8fabc4d7488d5f9eefc2b2aefecd
KK <krishnakumarkrish@gmail.com>
2024-03-08 09:46:52 +0530

    Changed Grup6.py

--- src/main/python/Group6.py
+++ src/main/python/Group6.py
@@ -1,3 +1,18 @@
+"""
+This is a simple module to demonstrate a hello world function in Python.
+"""
+
 import sys
+
 def Group6(out):
+    """
+    Print a message.
+
+    Args:
+        out: Output stream to write the message to.
+    """
+
     out.write("Assignment Pending\n")
+
+# Call the function with a file object
+Group6(sys.stdout)
